b40bfeed86962eaa615f9de8a944f2de453481f4
Fernando Bahamondes <ferbaco@gmail.com>
2021-03-07 18:53:50 -0300

    Add actions for subject controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,18 @@
 class SubjectsController < ApplicationController
+  def index
+    @subjects = Subject.all
+    render json: @subjects
+  end
+  
+  
+  def create
+    @subject = Session.create(subject_params)
+    render json: @subject
+  end
+
+
+  def subject_params
+    params.require(:subject).permit(:name, :time)
+  end
+end
 end
