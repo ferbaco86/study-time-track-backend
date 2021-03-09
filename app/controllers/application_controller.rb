6d7cfd44314b309d8a19007e0db206ea7ca88ade
Fernando Bahamondes <ferbaco@gmail.com>
2021-03-08 21:09:41 -0300

    Add login related actions to app controller. Add skip before filters to user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,39 @@
 class ApplicationController < ActionController::API
+  before_action :require_login
   def encode_token
     JWT.encode(payload, 'secret')
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'secret', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        []
+      end
+    end
+  end
+
+  def session_user
+    decoded_hash = decoded_token
+    unless decoded_hash.empty?
+      user_id = decoded_hash[0]['user_id']
+      @user = User.find(id: user_id)
+    else
+      nil
+    end
+  end
+
+  def logged_in?
+    !!session_user
+  end
+
+  def require_login
+    render json: {message: 'Loggin in is required'}, status: :unauthorized unless logged_in?
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:create]
   def index
     users = User.all
     render json: users
