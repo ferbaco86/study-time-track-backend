9e65ca9336a6221d3b05c4109f156ca763945b7f
Fernando Bahamondes <ferbaco@gmail.com>
2021-04-02 18:56:56 -0300

    Add test for session scope

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Session, type: :model do
   it { should have_many(:subjects) }
    # Validation tests
   it { should validate_presence_of(:title) }
+
+  describe "scopes" do
+    it "returns session subjects total time" do
+      session = create(:session)
+      session_subjects = create(:subject, name: 'test', time: 5, session_id: session.id)
+      expect(Session.subjects_total_time(session.user_id)).to contain_exactly(session)
+    end
+  end
 end
