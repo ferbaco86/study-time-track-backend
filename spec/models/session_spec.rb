dbd41ef309d111d55187a13fb72290f5ec0a3915
Fernando Bahamondes <ferbaco@gmail.com>
2021-04-01 22:27:05 -0300

    Add asociations and validations tests for models

---
+++ spec/models/session_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Session, type: :model do
+  # Association test
+  it { should belong_to(:user) }
+  it { should have_many(:subjects) }
+   # Validation tests
+  it { should validate_presence_of(:title) }
+
+ 
+end
